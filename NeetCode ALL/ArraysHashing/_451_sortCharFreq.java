91982ded6908a470dd2e13290ba35fa353134715
Piyush Raj <piyushraj4734@gmail.com>
2026-08-02 20:22:01 +0530

    Neetcode all array hash

---
+++ NeetCode ALL/ArraysHashing/_451_sortCharFreq.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String frequencySort(String s) {
+        Map<Character,Integer> freq = new HashMap<>();
+        for(char c : s.toCharArray()){
+            if(freq.containsKey(c)){
+                freq.put(c,freq.get(c)+1);
+            }else{
+                freq.put(c,1);
+            }
+        }
+
+        //list contains pair. 
+        // notes.
+        List<Map.Entry<Character,Integer>> list = new ArrayList<>(freq.entrySet());
+        list.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
+
+        StringBuilder sb = new StringBuilder();
+        for(Map.Entry<Character,Integer> entry : list){
+            int loop = entry.getValue();
+            for(int i = 0 ; i < loop ; i++){
+                sb.append(entry.getKey());
+            }
+        }
+        return sb.toString();
+    }
+}
---
+++ NeetCode ALL/ArraysHashing/_791_custom_sort_string.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String customSortString(String order, String s) {
+        // count freq of all char of s.
+        int[] arr = new int[26];
+        for(char c : s.toCharArray()){
+            arr[c-'a']++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(char c : order.toCharArray()){
+            if(arr[c-'a'] != 0){
+                for(int i = 0 ; i < arr[c-'a'] ; i++){
+                    sb.append(c);
+                }
+                arr[c-'a'] = 0;
+            }
+        }
+
+        for(int i = 0 ; i <= 25 ; i++){
+            if(arr[i] != 0){
+                for(int j = 0 ; j < arr[i] ; j++){
+                    sb.append( (char) (i +'a'));
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
